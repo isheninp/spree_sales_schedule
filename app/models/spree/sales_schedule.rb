4412388dce9487f08272699640045d461d5778ca
Pavel Ishenin <isheninp@gmail.com>
2019-01-14 18:29:12 +0300

    1401

--- app/models/spree/sales_schedule.rb
+++ app/models/spree/sales_schedule.rb
@@ -1,14 +1,25 @@
 class Spree::SalesSchedule < ActiveRecord::Base
   belongs_to :variant
   validates :start_time, :end_time, presence: true
+  #validate :validate_correct_string
   validate :validate_correct_period
   validate :validate_past_period
 
+
   private
 
+  def validate_correct_string
+    if !(Date.strptime(start_time,'%Y-%m-%d %H:%M') rescue nil)
+      errors.add(:start_time, I18n.t('sales_shedule.validate_correct_string_error'))
+    end
+    if !(Date.strptime(end_time,'%Y-%m-%d %H:%M') rescue nil)
+      errors.add(:end_time, I18n.t('sales_shedule.validate_correct_string_error'))
+    end  
+  end
+  
   def validate_correct_period
     if end_time < start_time
-      errors.add(:end_time, I18n.t('sales_schedule.validate_correct_period_error'))
+      errors.add(:end_time, I18n.t('sales_shedule.validate_correct_period_error'))
     end
   end
 
